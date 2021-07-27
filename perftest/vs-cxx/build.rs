a6555ac216f9e08f9fa063047bd6904b5c703527
Robert Collins <robert.collins@cognite.com>
2021-07-27 12:21:25 +0200

    Just the essentials (see bazelbuild bug 433)

--- perftest/vs-cxx/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    protobuf_codegen_pure::Codegen::new()
-        .out_dir(".")
-        .include(".")
-        .input("perftest_data.proto")
-        .run()
-        .expect("protoc");
-}
