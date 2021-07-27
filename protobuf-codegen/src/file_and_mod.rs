9b9a414e04ddc857cf526176a82fb68e576ea5a8
Robert Collins <robert.collins@cognite.com>
2021-07-27 12:16:42 +0200

    Just protobuf thanks

--- protobuf-codegen/src/file_and_mod.rs
+++
@@ -1,8 +0,0 @@
-use crate::customize::Customize;
-use crate::rust_name::RustRelativePath;
-
-pub(crate) struct FileAndMod {
-    pub file: String,
-    pub relative_mod: RustRelativePath,
-    pub customize: Customize,
-}
